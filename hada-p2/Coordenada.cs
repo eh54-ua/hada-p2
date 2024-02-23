d35b991623dcb2155535fbd9529a1c9a6b47aa01
Ergin Haxhijaj <eh54@alu.ua.es>
2024-02-23 12:27:05 +0100

    X7306964W Se añaden los métodos de la clase Coordenada

--- hada-p2/Coordenada.cs
+++ hada-p2/Coordenada.cs
@@ -11,26 +11,53 @@ namespace Hada
         public int Fila { get; set; }
         public int Columna { get; set; }
 
+        // Constructor por defecto
         public Coordenada()
         {
             this.Fila = 0;
             this.Columna = 0;
         }
+        // Constructor con dos números
         public Coordenada(int fila, int columna)
         {
             this.Fila = fila;
             this.Columna = columna;
         }
+        // Constructor con dos números en formato cadena
         public Coordenada(string fila, string columna)
         {
             this.Fila = int.Parse(fila);
             this.Columna = int.Parse(columna);
         }
+        // Constructor de copia
         public Coordenada(Coordenada c)
         {
             this.Fila = c.Fila;
             this.Columna = c.Columna;
         }
-
+        
+        // Muestra la coordenada por consola con el formato (fila,columna)
+        public override string ToString()
+        {
+            return "(" + Fila + "," + Columna + ")";
+        }
+        // Devuelve un código hash para poder hacer comparaciones en un diccionario
+        public override int GetHashCode()
+        {
+            return this.Fila.GetHashCode() ^ this.Columna.GetHashCode();
+        }
+        // Comparación con un objeto tipo Object
+        public override bool Equals(object obj)
+        {
+            if (obj != null && (obj is Coordenada))
+                return this.Fila == ((Coordenada)obj).Fila && this.Columna == ((Coordenada)obj).Columna;
+            else
+                return false;
+        }
+        // Comparación con un objeto tipo Coordenada
+        public bool Equals(Coordenada c)
+        {
+            return this.Fila == c.Fila && this.Columna == c.Columna;
+        }
     }
 }
