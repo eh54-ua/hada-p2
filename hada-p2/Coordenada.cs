38573a34982787ebee02dd0385a5f1c453d2fafd
Ergin Haxhijaj <eh54@alu.ua.es>
2024-02-23 11:59:21 +0100

    X7306964W Se añaden los constructores de la clase Coordenada

--- hada-p2/Coordenada.cs
+++ hada-p2/Coordenada.cs
@@ -8,5 +8,29 @@ namespace Hada
 {
     class Coordenada
     {
+        public int Fila { get; set; }
+        public int Columna { get; set; }
+
+        public Coordenada()
+        {
+            this.Fila = 0;
+            this.Columna = 0;
+        }
+        public Coordenada(int fila, int columna)
+        {
+            this.Fila = fila;
+            this.Columna = columna;
+        }
+        public Coordenada(string fila, string columna)
+        {
+            this.Fila = int.Parse(fila);
+            this.Columna = int.Parse(columna);
+        }
+        public Coordenada(Coordenada c)
+        {
+            this.Fila = c.Fila;
+            this.Columna = c.Columna;
+        }
+
     }
 }
