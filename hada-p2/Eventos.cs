7b7368ed757e5e6513b0bfd22da82e0eee47a31f
djfj1-ua <djfj1@alu.ua.es>
2024-02-29 15:04:57 +0100

    Añadidos los metodos de la clase Barco, fallo en eventos

---
+++ hada-p2/Eventos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hada
+{
+    class Eventos
+    {
+
+        public event EventHandler<TocadoArgs> eventoTocado;
+        //public event EventHandler<HundidoArgs> eventoHundido;
+
+        public class TocadoArgs : EventArgs
+        {
+            public string nombre { get; set; }
+            public string etiqueta { get; set; }
+            public Coordenada coordenadaImpacto { get; set; }
+
+            public TocadoArgs(string nombre, string etiqueta, Coordenada coordenadaImpacto)
+            {
+                this.nombre = nombre;
+                this.etiqueta = etiqueta;
+                this.coordenadaImpacto = coordenadaImpacto;
+            }
+        }
+
+        public void manejadorEventoTocado(object sender, TocadoArgs e)
+        {
+            Console.WriteLine("Evento tocado");
+        }
+
+    }
+}
